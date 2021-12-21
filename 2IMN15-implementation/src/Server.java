7eee027b341a3c3f79b746f6b082f6ab93375090
Milan van Gool <m.v.gool@student.tue.nl>
2021-12-21 16:35:27 +0100

    Remade project, not fixed :(

--- 2IMN15-implementation/src/Server.java
+++ 2IMN15-implementation/src/Server.java
@@ -2,6 +2,7 @@ import org.eclipse.leshan.server.californium.LeshanServerBuilder;
 import org.eclipse.leshan.server.californium.impl.LeshanServer;
 
 public class Server {
+
     public static void setupServer() {
         LeshanServerBuilder builder = new LeshanServerBuilder();
         LeshanServer server = builder.build();
