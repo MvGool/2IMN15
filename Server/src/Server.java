2ff46b0e1ce6775b1871956ee4dc8a0d1f6b11be
Milan van Gool <m.v.gool@student.tue.nl>
2021-12-21 14:48:40 +0100

    Added main file

---
+++ Server/src/Server.java
@@ -0,0 +1,14 @@
+import org.eclipse.leshan.server.californium.LeshanServer;
+import org.eclipse.leshan.server.californium.LeshanServerBuilder;
+
+public class Server {
+    public static void setupServer() {
+        LeshanServerBuilder builder = new LeshanServerBuilder();
+        LeshanServer server = builder.build();
+        server.start();
+    }
+
+    public static void main(String args[]) {
+        setupServer();
+    }
+}
