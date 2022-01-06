ce160fa4e77f2282a5e5d9c3301b6b45364b75b0
Milan van Gool <m.v.gool@student.tue.nl>
2022-01-06 13:35:24 +0100

    Start has been made

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,7 +1,10 @@
 package server;
 
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.Collection;
 
+import org.eclipse.californium.elements.config.Configuration;
 import org.eclipse.leshan.core.node.LwM2mResource;
 import org.eclipse.leshan.core.observation.Observation;
 import org.eclipse.leshan.core.request.ReadRequest;
@@ -11,30 +14,68 @@ import org.eclipse.leshan.server.californium.LeshanServerBuilder;
 import org.eclipse.leshan.server.registration.Registration;
 import org.eclipse.leshan.server.registration.RegistrationListener;
 import org.eclipse.leshan.server.registration.RegistrationUpdate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Server {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(Server.class);
+	private static final String CF_CONFIGURATION_FILENAME = "Californium.properties";
+	private static final String CF_CONFIGURATION_HEADER = "Leshan Server Demo";
+	
+//	public static LeshanServer createLeshanServer(ServerCLI cli) throws Exception {
+	public static LeshanServer createLeshanServer() throws Exception {
+		LeshanServerBuilder builder = new LeshanServerBuilder();
+		File configFile = new File(CF_CONFIGURATION_FILENAME);
+		// TODO: configuration stuff
+		Configuration coapConfig = LeshanServerBuilder.createDefaultCoapConfiguration();
+
+		// TODO: cli stuff
+//		builder.setLocalAddress(cli.main.localAddress,
+//				cli.main.localPort == null ? coapConfig.get(CoapConfig.COAP_PORT) : cli.main.localPort);
+//		builder.setLocalSecureAddress(cli.main.secureLocalAddress,
+//				cli.main.secureLocalPort == null ? coapConfig.get(CoapConfig.COAP_SECURE_PORT)
+//						: cli.main.secureLocalPort); // Create DTLS Config
+//		
+		return builder.build();
+	}
+	
 	
 	public static void main(String[] args) {
-		LeshanServerBuilder builder = new LeshanServerBuilder();
-		final LeshanServer server = builder.build();
-		server.start();
-		
-		server.getRegistrationService().addListener(new RegistrationListener() {
 
+		try {
+			// Create LWM2M Server
+			LeshanServer lwm2mServer = createLeshanServer(); // Create Web Server
+//			Server webServer = createJettyServer(cli, lwm2mServer); // Register a service to DNS-SD
+//			if (cli.main.mdns != null) { // Create a JmDNS instance
+//				JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost()); // Publish Leshan HTTP Service
+//				ServiceInfo httpServiceInfo = ServiceInfo.create("_http._tcp.local.", "leshan", cli.main.webPort, "");
+//				jmdns.registerService(httpServiceInfo); // Publish Leshan CoAP Service
+//				ServiceInfo coapServiceInfo = ServiceInfo.create("_coap._udp.local.", "leshan", cli.main.localPort, "");
+//				jmdns.registerService(coapServiceInfo); // Publish Leshan Secure CoAP Service
+//				ServiceInfo coapSecureServiceInfo = ServiceInfo.create("_coaps._udp.local.", "leshan",
+//						cli.main.secureLocalPort, "");
+//				jmdns.registerService(coapSecureServiceInfo);
+//			} // Start servers
+			lwm2mServer.start();
+//			webServer.start();
+//			LOG.info("Web server started at {}.", webServer.getURI());
+			registerEventListeners(lwm2mServer);
+		} catch (Exception e) { // Handler Execution Error
+//			PrintWriter printer = command.getErr();
+//			printer.print(command.getColorScheme().errorText("Unable to create and start server ..."));
+//			printer.printf("%n%n");
+//			printer.print(command.getColorScheme().stackTraceText(e));
+//			printer.flush();
+			System.exit(1);
+		}
+	}
+		
+	public static void registerEventListeners(LeshanServer server) {
+		server.getRegistrationService().addListener(new RegistrationListener() {
 		    public void registered(Registration registration, Registration previousReg,
 		            Collection<Observation> previousObsersations) {
 		    	System.out.println("new device: " + registration.getEndpoint());
-			    try {
-			        ReadResponse response = server.send(registration, new ReadRequest(3,0,13));
-			        if (response.isSuccess()) {
-			            System.out.println("Device time:" + ((LwM2mResource)response.getContent()).getValue());
-			        }else {
-			            System.out.println("Failed to read:" + response.getCode() + " " + response.getErrorMessage());
-			        }
-			    } catch (InterruptedException e) {
-			        e.printStackTrace();
-			    }
 			}
 
 		    public void updated(RegistrationUpdate update, Registration updatedReg, Registration previousReg) {
